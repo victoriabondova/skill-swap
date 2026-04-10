1f25fae94f353973ea3422b6ac4dd3b02da193b6
Victoria Bondova <victoriabondova9@gmail.com>
2026-04-10 23:45:20 +0300

    Define ISkill and IUser interfaces

---
+++ src/app/interfaces/skill.interface.ts
@@ -0,0 +1,10 @@
+export interface ISkill {
+    _id: string;
+    title: string;
+    category: string;
+    description: string;
+    lookingFor: string;
+    ownerId: string;
+    ownerName: string;
+    createdAt: number;
+}
