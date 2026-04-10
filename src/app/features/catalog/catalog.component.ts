f56b299cc81b9796699442fcd6c5473f2e323de7
Victoria Bondova <victoriabondova9@gmail.com>
2026-04-11 00:19:15 +0300

    Implement basic HTML structure for Home, Catalog, login and register

---
+++ src/app/features/catalog/catalog.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog',
+  imports: [],
+  templateUrl: './catalog.component.html',
+  styleUrl: './catalog.component.css',
+})
+export class CatalogComponent {}
