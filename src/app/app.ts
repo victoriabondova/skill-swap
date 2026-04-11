eaedc0528f227859384d420c36a2b9db9b542f87
Victoria Bondova <victoriabondova9@gmail.com>
2026-04-11 09:48:28 +0300

    Add Footer and set up main content area

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
-//import { FooterComponent } from './core/footer/footer.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, /*FooterComponent*/],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
